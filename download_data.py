adb8e8cf097f7257f911fb4696e489a7c140aa8f
jbm91 <jonasmathiassen@gmail.com>
2021-06-10 10:17:25 +0200

    Will now only get files not already downloaded

--- download_data.py
+++ download_data.py
@@ -1,6 +1,7 @@
 import paramiko
 import numpy as np
-
+from glob import glob
+import os 
 paramiko.util.log_to_file("paramiko.log")
 
 # Open a transport
@@ -17,9 +18,19 @@ sftp = paramiko.SFTPClient.from_transport(transport)
 # Get list of files
 files = sftp.listdir()
 
+
+localPath = "/home/s200987/Documents/Jonas/appml/raw_data/"
+localFiles = [os.path.basename(x) for x in glob(localPath+'*.txt')]
+def Diff(li1, li2):
+    return list(set(li1) - set(li2))
+
+filesToGet = Diff(files, localFiles)
+
+
+#%%
 # Save the files to 
 from tqdm import tqdm
-for file in tqdm(files):
+for file in tqdm(filesToGet):
     data_string = sftp.open(file).read().decode('utf-8')
     data = np.array([np.array(i.split(' ')).astype('float') for i in data_string.split('\n')[:-1]]).transpose()
     np.savetxt("/home/s200987/Documents/Jonas/appml/raw_data/"+file,data)
